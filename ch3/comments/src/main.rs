34832ff4425079351d89fe88a601c1c01e7050ee
Lars Jansøn Engvik <lars@engvik.nu>
2020-09-15 20:23:10 +0200

    feature(ch3): comments

---
+++ ch3/comments/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    // Hello, world
+    println!("Hello, world!");
+
+    // Long comment. Long long comment. Long long long comment. Long long long
+    // long comment.
+
+    let number = 5; // This is the number five.
+}
