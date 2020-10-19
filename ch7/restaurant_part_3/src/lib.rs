a50a2c7b67de2417de149447ce0946af11cc5b49
Lars Jansøn Engvik <lars@engvik.nu>
2020-10-19 14:29:09 +0200

    feature(ch7): bringing paths into scope with the use keyword

---
+++ ch7/restaurant_part_3/src/lib.rs
@@ -0,0 +1,37 @@
+mod front_of_house {
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
+    }
+}
+
+use crate::front_of_house::hosting;
+// use self::front_of_house::hosting;
+// use crate::front_of_house::hosting::add_to_waitlist;
+// pub use crate::front_of_house::hosting;
+
+pub fn eat_at_restaurant() {
+    hosting::add_to_waitlist();
+    hosting::add_to_waitlist();
+    hosting::add_to_waitlist();
+    // add_to_waitlist();
+    // add_to_waitlist();
+    // add_to_waitlist();
+}
+
+/*
+use std::fmt;
+use std::io;
+
+fn function1() -> fmt::Result {}
+
+fn function2() -> io::Result<()> {}
+*/
+
+/*
+use std::fmt::Result;
+use std::io:Result as IoResult;
+
+fn function1() -> Result {}
+
+fn function2() -> IoResult<()> {}
+*/
---
+++ ch7/restaurant_part_3/src/main.rs
@@ -0,0 +1,21 @@
+use rand::Rng;
+use std::collections::HashMap;
+
+// use std::cmp::Ordering;
+// use std::io;
+// =
+// use std::{cmp::Ordering, io};
+
+// use std::io;
+// use std::io::Write;
+// =
+// use std::io::{self, Write};
+
+// std::collections::*;
+
+fn main() {
+    let mut map = HashMap::new();
+    map.insert(1, 2);
+
+    let _secret_number = rand::thread_rng().gen_range(1, 101);
+}
