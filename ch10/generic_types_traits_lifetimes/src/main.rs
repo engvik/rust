5dceba73fd4b4b3faaeacfc461ab318c1847f22c
Lars Jansøn Engvik <lars@engvik.nu>
2020-12-01 20:10:28 +0100

    feature(ch10): generic types, traits, and lifetimes

---
+++ ch10/generic_types_traits_lifetimes/src/main.rs
@@ -0,0 +1,43 @@
+fn largest(list: &[i32]) -> &i32 {
+    let mut largest = &list[0];
+
+    for item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
+
+fn main() {
+    let number_list = vec![34, 50, 25, 100, 65];
+    let mut largest_number = number_list[0];
+
+    for number in number_list {
+        if number > largest_number {
+            largest_number = number;
+        }
+    }
+
+    println!("The largest number is {}", largest_number);
+
+    let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
+    let mut largest_number = number_list[0];
+
+    for number in number_list {
+        if number > largest_number {
+            largest_number = number;
+        }
+    }
+
+    println!("The largest number is {}", largest_number);
+
+    let number_list = vec![34, 50, 25, 100, 65];
+    let result = largest(&number_list);
+    println!("The largest number is {}", result);
+
+    let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
+    let result = largest(&number_list);
+    println!("The largest number is {}", result);
+}
