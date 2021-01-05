0b8287c2afc31de1be6324bc3186f59c7b98e3e4
Lars Jansøn Engvik <lars@engvik.nu>
2021-01-05 22:20:14 +0100

    feature(ch10): validating references with lifetimes

---
+++ ch10/lifetimes/src/main.rs
@@ -0,0 +1,114 @@
+use std::fmt::Display;
+
+struct ImportantExcerpt<'a> {
+    part: &'a str,
+}
+
+impl<'a> ImportantExcerpt<'a> {
+    fn level(&self) -> i32 {
+        3
+    }
+
+    fn announce_and_return_part(&self, announcement: &str) -> &str {
+        println!("Attention please: {}", announcement);
+        self.part
+    }
+}
+
+fn main() {
+    /*
+    {
+        let r;
+
+        {
+            let x = 5;
+            r = &x;
+        }
+
+        println!("r: {}", r)
+    }
+    */
+    let x = 5;
+    let r = &x;
+
+    println!("r: {}", r);
+
+    let string1 = String::from("abcd");
+    let string2 = "xyz";
+
+    let result = longest(string1.as_str(), string2);
+    println!("The longest string is {}", result);
+
+    let string1 = String::from("long string is long");
+
+    {
+        let string2 = String::from("xyz");
+        let result = longest(string1.as_str(), string2.as_str());
+        println!("The longest string is {}", result);
+    }
+
+    /*
+    let string1 = String::from("long string is long");
+    let result;
+    {
+        let string2 = String::from("xyz");
+        result = longest(string1.as_str(), string2.as_str());
+    }
+    println!("The longest string is {}", result);
+    */
+
+    let novel = String::from("Call me Ishmael. Some years ago...");
+    let first_sentence = novel.split('.').next().expect("Could not find a '.'");
+    let i = ImportantExcerpt {
+        part: first_sentence,
+    };
+
+    let part = i.announce_and_return_part("hello!");
+    println!("part: {}, level {}", part, i.level());
+
+    let string1 = String::from("long string is long");
+    let string2 = String::from("xyz");
+    let result = longest_with_an_announcement(string1.as_str(), string2.as_str(), "Hello!");
+    println!("The longest string is {}", result);
+}
+
+/*
+fn longest(x: &str, y: &str) -> &str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
+*/
+
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
+
+fn _longest2<'a>(x: &'a str, _y: &'a str) -> &'a str {
+    x
+}
+
+/*
+fn longest3<'a>(_x: &'a str, _y: &'a str) -> &'a str {
+    let result = String::from("really long string");
+    result.as_str()
+}
+*/
+
+fn longest_with_an_announcement<'a, T>(x: &'a str, y: &'a str, ann: T) -> &'a str
+where
+    T: Display,
+{
+    println!("Announcement! {}", ann);
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
