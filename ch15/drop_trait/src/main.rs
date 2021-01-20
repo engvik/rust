a507c6a590f96d3d2070a2cbd6c6c43b76f81784
Lars Jansøn Engvik <lars@engvik.nu>
2021-01-20 21:13:26 +0100

    feature(ch15): running code on cleanup with the drop trait

---
+++ ch15/drop_trait/src/main.rs
@@ -0,0 +1,33 @@
+struct CustomSmartPointer {
+    data: String,
+}
+
+impl Drop for CustomSmartPointer {
+    fn drop(&mut self) {
+        println!("Dropping CustomSmartPointer with data `{}́ !", self.data);
+    }
+}
+
+fn main() {
+    let _c = CustomSmartPointer {
+        data: String::from("my stuff"),
+    };
+    let _d = CustomSmartPointer {
+        data: String::from("other stuff"),
+    };
+    println!("CustomSmartPointers created.");
+
+    let _c = CustomSmartPointer {
+        data: String::from("some data"),
+    };
+    println!("CustomSmartPointer created.");
+    // c.drop();
+    println!("CustomSmartPointer dropped before the end of main.");
+
+    let c = CustomSmartPointer {
+        data: String::from("some data"),
+    };
+    println!("CustomSmartPointer created.");
+    drop(c);
+    println!("CustomSmartPointer dropped before the end of main.");
+}
