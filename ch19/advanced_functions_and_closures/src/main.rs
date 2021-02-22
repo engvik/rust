76e18e9a49dfb417ee9bc023d8ee21c2d308b4aa
Lars Jansøn Engvik <lars@engvik.nu>
2021-02-22 16:33:54 +0100

    feature(ch19): advanced functions and closures

---
+++ ch19/advanced_functions_and_closures/src/main.rs
@@ -0,0 +1,38 @@
+fn add_one(x: i32) -> i32 {
+    x + 1
+}
+
+fn do_twice(f: fn(i32) -> i32, arg: i32) -> i32 {
+    f(arg) + f(arg)
+}
+
+#[derive(Debug)]
+enum Status {
+    Value(u32),
+    _Stop,
+}
+
+/*
+fn returns_closure() -> dyn Fn(i32) -> i32 {
+    |x| x + 1
+}
+*/
+
+fn returns_closure() -> Box<dyn Fn(i32) -> i32> {
+    Box::new(|x| x + 1)
+}
+
+fn main() {
+    let answer = do_twice(add_one, 5);
+    println!("The answer is: {}", answer);
+
+    let list_of_numbers = vec![1, 2, 3];
+    let list_of_strings: Vec<String> = list_of_numbers.iter().map(|i| i.to_string()).collect();
+    println!("list_of_numbers: {:?}", list_of_numbers);
+    println!("list_of_strings: {:?}", list_of_strings);
+
+    let list_of_statuses: Vec<Status> = (0u32..20).map(Status::Value).collect();
+    println!("list_of_statuses: {:?}", list_of_statuses);
+
+    println!("closure: {}", returns_closure()(2))
+}
